27b28653b18c5df2b199de82e93a562b75a79a97
Nikolay Nazarov <mgsmile90@ya.ru>
2019-01-14 14:19:59 +0300

    Update dev deps and settings for gulp. autoprefixer, sourcemaps, concat and uglify(for js) was added

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ const sass = require('gulp-sass');
 const spritesmith = require('gulp.spritesmith');
 const rimraf = require('rimraf');
 const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 
 // Server
 gulp.task('server', function() {
@@ -31,8 +35,28 @@ gulp.task('templates:compile', function buildHTML() {
 gulp.task('styles:compile', function () {
   return gulp.src('src/styles/main.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 10 versions'],
+      cascade: false
+    }))
     .pipe(rename('main.min.css'))
     .pipe(gulp.dest('build/css'));
+});
+
+/* --------  js -------- */
+gulp.task('js', function() {
+  return gulp.src([
+          'src/js/init.js',
+          'src/js/validation.js',
+          'src/js/form.js',
+          'src/js/navigation.js',
+          'src/js/main.js'
+      ])
+      .pipe(sourcemaps.init())
+      .pipe(concat('main.min.js'))
+      .pipe(uglify())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('build/js'));
 });
 
 /* ----------- Spritesmith for imgs ------------ */
@@ -72,11 +96,12 @@ gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
 gulp.task('watch', function() {
   gulp.watch('src/templates/**/*.pug', gulp.series('templates:compile'));
   gulp.watch('src/styles/**/*.scss', gulp.series('styles:compile'));
+  gulp.watch('source/js/**/*.js', gulp.series('js'));
 });
 
 gulp.task('default', gulp.series(
   'clean',
-  gulp.parallel('templates:compile', 'styles:compile', 'sprite', 'copy'),
+  gulp.parallel('templates:compile', 'styles:compile', 'js', 'sprite', 'copy'),
   gulp.parallel('watch', 'server')
   )
 );
